edfb4d53cf993c6475fbce724659e5df34112502
HilariusRyanAB <benedecash@gmail.com>
2023-10-25 21:45:37 +0700

    [UPD] Dashboard Pangkalan

--- src/components/DashboardPage.vue
+++ src/components/DashboardPage.vue
@@ -51,7 +51,7 @@
 
             <div v-else>
                 <dashboard-pangkalan 
-                    :monthNow="monthNow"
+                    :monthNow="monthNow" :items="pangkalanItems"
                 />
             </div>
                         
@@ -348,6 +348,68 @@
                 users: [],
 
                 //Pangkalan Variable
+                pangkalanItems: [
+                    {
+                        avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+                        title: 'Brunch this weekend?',
+                        subtitle: `<span class="text--primary">Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?`,
+                    },
+                    { divider: true, inset: true },
+                    {
+                        avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
+                        title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
+                        subtitle: `<span class="text--primary">to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend.`,
+                    },
+                    { divider: true, inset: true },
+                    {
+                        avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
+                        title: 'Oui oui',
+                        subtitle: '<span class="text--primary">Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?',
+                    },
+                    { divider: true, inset: true },
+                    {
+                        avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg',
+                        title: 'Birthday gift',
+                        subtitle: '<span class="text--primary">Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?',
+                    },
+                    { divider: true, inset: true },
+                    {
+                        avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
+                        title: 'Recipe to try',
+                        subtitle: '<span class="text--primary">Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.',
+                    },
+                    { divider: true, inset: true },
+                    {
+                        avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+                        title: 'Brunch this weekend?',
+                        subtitle: `<span class="text--primary">Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?`,
+                    },
+                    { divider: true, inset: true },
+                    {
+                        avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
+                        title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
+                        subtitle: `<span class="text--primary">to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend.`,
+                    },
+                    { divider: true, inset: true },
+                    {
+                        avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
+                        title: 'Oui oui',
+                        subtitle: '<span class="text--primary">Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?',
+                    },
+                    { divider: true, inset: true },
+                    {
+                        avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg',
+                        title: 'Birthday gift',
+                        subtitle: '<span class="text--primary">Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?',
+                    },
+                    { divider: true, inset: true },
+                    {
+                        avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
+                        title: 'Recipe to try',
+                        subtitle: '<span class="text--primary">Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.',
+                    },
+                    { divider: true, inset: true },
+                ],
             }
         },
 
