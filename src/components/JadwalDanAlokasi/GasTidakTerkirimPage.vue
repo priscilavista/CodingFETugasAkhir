3dac0b4118d5cc17d359796700d5f29e445f3a78
priscilavista <vistapriscila@gmail.com>
2024-03-10 23:49:37 +0700

    [UPD] gas tidak terkirim

--- src/components/JadwalDanAlokasi/GasTidakTerkirimPage.vue
+++ src/components/JadwalDanAlokasi/GasTidakTerkirimPage.vue
@@ -19,61 +19,118 @@
       <h3 style="float:left">Gas Tidak Terkirim</h3>
     </div>
 
-    <v-card fill-height class="flex-item mx-auto" elevation="5" style="margin-top: 5%">
-      <v-card-title class="text-right" style="display: inherit;">
-        <v-text-field
-          v-if="isWideScreen"
-          v-model="search"
-          append-icon="mdi-magnify"
-          label="Cari"
-          single-line
-          hide-details
-          style="margin-left: 75%; width: 25%;"
-        />
+    <v-container style="padding: 0px 0px 12px 0px; margin-left:0px">
+      <v-row>
+        <v-col
+          cols="12"
+          sm="6"
+          md="6"
+        >
+          <v-card width="350px" style="background: #196b4d; border-radius: 4px 4px 0px 0px">
+            <v-card-title>
+              <h3 style="font-size:20px; color:#ffffff">Harus Diproses</h3>
+            </v-card-title>
+          </v-card>
+          <v-card fill-height class="flex-item mx-auto" elevation="5">
+            <v-card-title class="text-right" style="display: inherit;">
+              <v-text-field
+                v-if="isWideScreen"
+                v-model="search"
+                append-icon="mdi-magnify"
+                label="Cari"
+                single-line
+                hide-details
+                style="margin-left: 75%; width: 25%;"
+              />
 
-        <v-text-field
-          v-else
-          v-model="search"
-          append-icon="mdi-magnify"
-          label="Cari"
-          single-line
-          hide-details
-          style="margin-left: 50%; width: 50%;"
-        />
-      </v-card-title>
+              <v-text-field
+                v-else
+                v-model="search"
+                append-icon="mdi-magnify"
+                label="Cari"
+                single-line
+                hide-details
+                style="margin-left: 50%; width: 50%;"
+              />
+            </v-card-title>
 
-      <v-data-table
-        :headers="headers"
-        :items="jadwal"
-        :search="search"
-        style="padding: 10px 20px 20px 20px"
-      >
-        <template v-slot:[`item.actions`]="{ item }">
-          <v-menu offset-y style="float: left">
-            <template v-slot:activator="{ on, attrs }">
-              <span v-bind="attrs" v-on="on" style="cursor: pointer">
-                <v-tooltip top>
+            <v-data-table
+              :headers="headers"
+              :items="jadwalRequest"
+              :search="search"
+              style="padding: 10px 20px 20px 20px"
+            >
+              <template v-slot:[`item.actions`]="{ item }">
+                <v-menu offset-y style="float: left">
                   <template v-slot:activator="{ on, attrs }">
-                    <v-icon v-bind="attrs" v-on="on" color="primary" @click="confirmHandler(item)" style="margin-right: 15px;">
-                      mdi-check
-                    </v-icon>
+                    <span v-bind="attrs" v-on="on" style="cursor: pointer">
+                      <v-tooltip top>
+                        <template v-slot:activator="{ on, attrs }">
+                          <v-icon v-bind="attrs" v-on="on" color="primary" @click="confirmHandler(item)" style="margin-right: 15px;">
+                            mdi-check
+                          </v-icon>
+                        </template>
+                        <span>Terima Alokasi Fakultatif</span>
+                      </v-tooltip>
+                      <v-tooltip top>
+                        <template v-slot:activator="{ on, attrs }">
+                          <v-icon v-bind="attrs" v-on="on" @click="rejectHandler(item)" color="error">
+                            mdi-close
+                          </v-icon>
+                        </template>
+                        <span>Tolak Alokasi Fakultatif</span>
+                      </v-tooltip>
+                    </span>
                   </template>
-                  <span>Terima Alokasi Fakultatif</span>
-                </v-tooltip>
-                <v-tooltip top>
-                  <template v-slot:activator="{ on, attrs }">
-                    <v-icon v-bind="attrs" v-on="on" @click="rejectHandler(item)" color="error">
-                      mdi-close
-                    </v-icon>
-                  </template>
-                  <span>Tolak Alokasi Fakultatif</span>
-                </v-tooltip>
-              </span>
-            </template>
-          </v-menu>
-        </template>
-      </v-data-table>
-    </v-card>
+                </v-menu>
+              </template>
+            </v-data-table>
+          </v-card>
+        </v-col>
+        <v-col
+          cols="12"
+          sm="6"
+          md="6"
+        >
+          <v-card width="350px" style="background: #196b4d; border-radius: 4px 4px 0px 0px">
+            <v-card-title>
+              <h3 style="font-size:20px; color:#ffffff">Sudah Dikonfirmasi</h3>
+            </v-card-title>
+          </v-card>
+          <v-card fill-height class="flex-item mx-auto" elevation="5">
+            <v-card-title class="text-right" style="display: inherit;">
+              <v-text-field
+                v-if="isWideScreen"
+                v-model="search"
+                append-icon="mdi-magnify"
+                label="Cari"
+                single-line
+                hide-details
+                style="margin-left: 75%; width: 25%;"
+              />
+
+              <v-text-field
+                v-else
+                v-model="search"
+                append-icon="mdi-magnify"
+                label="Cari"
+                single-line
+                hide-details
+                style="margin-left: 50%; width: 50%;"
+              />
+            </v-card-title>
+
+            <v-data-table
+              :headers="headers2"
+              :items="jadwalConfirm"
+              :search="search"
+              style="padding: 10px 20px 20px 20px"
+            >
+            </v-data-table>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
 
     <v-dialog v-model="dialogConfirm" persistent max-width="400px">
       <v-card>
@@ -165,7 +222,8 @@
         search: null,
         overlay: false,
         snackbar: false,
-        jadwal: [],
+        jadwalRequest: [],
+        jadwalConfirm: [],
         error_message: "",
         dialogReject: false,
         dialogConfirm: false,
@@ -191,12 +249,25 @@
             sortable: true,
             value: "tanggal_pengambilan_gas",
           },
-          { text: "Jenis", value: "jenis_alokasi_pengambilan_gas" },
           { text: "Pangkalan", value: "nama_pangkalan" },
           { text: "Driver", value: "nama_driver" },
-          { text: "Jumlah Alokasi", value: "total_alokasi_penerimaan_gas" },
           { text: "Jumlah Tidak Terkirim", value: "gas_tidak_terkirim" },
+          { text: "Jumlah Alokasi", value: "total_alokasi_penerimaan_gas" },
           { text: "Konfirmasi", value: "actions", sortable: false },
+          { text: "Jenis", value: "jenis_alokasi_pengambilan_gas" },
+        ],
+        headers2: [
+          {
+            text: "Tanggal Pengiriman",
+            align: "start",
+            sortable: true,
+            value: "tanggal_pengambilan_gas",
+          },
+          { text: "Pangkalan", value: "nama_pangkalan" },
+          { text: "Driver", value: "nama_driver" },
+          { text: "Jumlah Tidak Terkirim", value: "gas_tidak_terkirim" },
+          { text: "Jumlah Alokasi", value: "total_alokasi_penerimaan_gas" },
+          { text: "Jenis", value: "jenis_alokasi_pengambilan_gas" },
         ],
         form: {
             Pangkalanid_pangkalan : null,
@@ -244,10 +315,11 @@
             if(response.data.code == 200)
             {
               var temp = response.data.data;
+              var jadwal = [];
 
               for (let i = 0; i < temp.length; i++)
               {
-                this.jadwal.push(
+                jadwal.push(
                 {
                     id_driver: temp[i].id_pegawai,
                     nama_driver: temp[i].nama_pegawai,
@@ -268,8 +340,17 @@
                     tanggal_penambahan_alokasi: '',
                     alokasi_tambahan: '',
                     status_persetujuan_pangkalan: '',
-                },
-            );
+                },);
+
+                jadwal.forEach(element => {
+                  if(element.status_gas_tidak_terkirim === 'R')
+                  {
+                    this.jadwalRequest = [...this.jadwalRequest, element];
+                  }
+                  else{
+                    this.jadwalConfirm = [...this.jadwalConfirm, element];
+                  }
+                });
               }
 
               this.color = "green";
@@ -303,10 +384,11 @@
             if(response.data.code == 200)
             {
               var temp = response.data.data;
+              var jadwal = [];
 
               for (let i = 0; i < temp.length; i++)
               {
-                this.jadwal.push(
+                jadwal.push(
                 {
                     id_driver: temp[i].id_pegawai,
                     nama_driver: temp[i].nama_pegawai,
@@ -327,8 +409,17 @@
                     status_persetujuan_pangkalan: temp[i].status_persetujuan_pangkalan,
                     id_jadwal_pengiriman_gas: '',
                     Jadwal_Tetap_Pangkalanid_kitir_pangkalan: '',
-                },
-            );
+                },);
+
+                jadwal.forEach(element => {
+                  if(element.status_gas_tidak_terkirim === 'R')
+                  {
+                    this.jadwalRequest = [...this.jadwalRequest, element];
+                  }
+                  else{
+                    this.jadwalConfirm = [...this.jadwalConfirm, element];
+                  }
+                });
               }
 
               this.color = "green";
